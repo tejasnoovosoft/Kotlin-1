10c5f6e9a19a0f560c54ebd2c0047682f9dc88df
tejasnoovosoft <tejas.noovosoft@gmail.com>
2023-10-06 13:18:59 +0530

    Refactor Functions of Kotlin Exercise

--- Exercise/src/main/kotlin/Program.kt
+++ Exercise/src/main/kotlin/Program.kt
@@ -8,43 +8,21 @@ fun normalizeDimension(value: Int): Int {
 
 fun createUnit(width: Int, length: Int, isStackable: Boolean, isStacked: Boolean): Map<String, Any> {
     return mapOf(
-        "width" to width, "length" to length, "isStackable" to isStackable, "isStacked" to isStacked
+        "width" to width,
+        "length" to length,
+        "isStackable" to isStackable,
+        "isStacked" to isStacked
     )
 }
 
-/*fun calculateNormalizedUnits(items: List<Item>): List<Map<String, Any>> {
-
-    val normalizedUnits = mutableListOf<Map<String, Any>>()
-    items.map { item ->
-        if (item.isStackable) {
-            val normalizedWidth = normalizeDimension(item.width)
-            val normalizedLength = normalizeDimension(item.length)
-
-            val halfQuantity = item.quantity / 2
-
-            repeat(halfQuantity) {
-                normalizedUnits.add(createUnit(normalizedWidth, normalizedLength, item.isStackable, true))
-            }
-
-            if (item.quantity % 2 != 0) {
-                normalizedUnits.add(createUnit(normalizedWidth, normalizedLength, item.isStackable, false))
-            }
-        } else {
-            repeat(item.quantity) {
-                normalizedUnits.add(createUnit(item.width, item.length, item.isStackable, false))
-            }
-        }
-    }
-    return normalizedUnits
-}*/
-
 fun List<Item>.calculateNormalizedUnits(): List<Map<String, Any>> {
     return this.flatMap { item ->
+
         val normalizedWidth = normalizeDimension(item.width)
         val normalizedLength = normalizeDimension(item.length)
         val halfQuantity = item.quantity / 2
 
-        val stackedUnits = if (item.isStackable) {
+        val stackedUnits = if (item.isStackable && item.quantity > 1) {
             List(halfQuantity) {
                 createUnit(normalizedWidth, normalizedLength, item.isStackable, true)
             }
@@ -71,7 +49,10 @@ fun List<Item>.calculateNormalizedUnits(): List<Map<String, Any>> {
 
 fun main() {
     val items = listOf(
-        Item(99, 35, true, 10), Item(85, 37, true, 5), Item(100, 55, false, 20), Item(45, 67, true, 17)
+        Item(99, 35, true, 10),
+        Item(85, 37, true, 5),
+        Item(100, 55, false, 20),
+        Item(45, 67, true, 17)
     )
     val normalizedUnits = items.calculateNormalizedUnits()
     normalizedUnits.forEach { println(it) }
